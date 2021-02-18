0da1ec2a3084e368bef747daf123ab9aa2cd1024
侯逸仙 <houyixian@sunlands.com>
2021-02-18 16:33:01 +0800

    增加空白项目

---
+++ OffscreenRenderingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OffscreenRenderingDemo
+//
+//  Created by 侯逸仙 on 2021/2/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
